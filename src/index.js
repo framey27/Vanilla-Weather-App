8f104aa0895b804c2bc67030938a49a19fa98cb1
framey27 <128307458+framey27@users.noreply.github.com>
2023-06-14 15:01:18 +0100

    Update the time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+function formatDate(timestamp) {
+    let date = new date(timestamps);
+    let hours = timestamp.getHours();
+     if (hours < 10) {
+        hours = '0${hours}'; 
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+        minutes = '0${minutes}'; 
+    }
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[day.getDay()];
+    return '${day} ${hours}:${minutes}';
+}
+
 function displayTemperature(response) {
     
     let temperatureElement = document.querySelector("#temperature");
@@ -5,11 +19,13 @@ function displayTemperature(response) {
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
+    let dateElement = document.querySelector("#date")
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
+    dateElement.innerHTML = timestamp(response.data.dt*1000);
 }
 
 let apiKey = "76fb06fa46a47159706d99bfcf4c874b";
